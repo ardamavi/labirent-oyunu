ebd62d6cc2a87c0a85f7cf903927a774d01e5d4f
Arda Mavi <ardamavi2@gmail.com>
2016-11-22 15:38:57 +0200

    Proje Dosyaların Eklenmesi

---
+++ Assets/Kodlar/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;
+	public int hiz = 25;
+
+	// Use this for initialization
+	void Start () {
+	
+		Debug.Log ("Oyun Başladı !");
+
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("escape")){
+			Debug.Log ("Oyun'dan Çıkılıyor ...");
+			Application.Quit();
+	     }
+
+		if (Input.GetKey ("w")) {
+			Debug.Log ("Yukarı");
+			rb.AddForce (transform.forward * hiz);
+		}
+		if (Input.GetKey ("s")) {
+			Debug.Log ("Aşağı");
+			rb.AddForce (transform.forward * -hiz);
+		}
+		if (Input.GetKey ("a")) {
+			Debug.Log ("Sol");
+			rb.AddForce (transform.right * -hiz);
+		}
+		if (Input.GetKey ("d")) {
+			Debug.Log ("Sağ");
+			rb.AddForce (transform.right * hiz);
+		}
+	
+	}
+
+	void OnCollisionEnter (Collision degen){
+	
+		if (degen.gameObject.tag == "Enemy") {
+		
+			Debug.Log ("Kaybettin !");
+
+			Destroy (gameObject);
+
+			Application.LoadLevel(Application.loadedLevel);
+		
+		}
+
+		if (degen.gameObject.tag == "Finish") {
+
+			Debug.Log ("Kazandın !");
+
+			Destroy (gameObject);
+
+			Application.LoadLevel (Application.loadedLevel + 1);
+
+		}
+	
+	}
+}
